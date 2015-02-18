926dfbee18737d224c70c4e094bd4966bf48c90a
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2015-02-17 20:01:31 -0500

    basic force layout working

---
+++ script.js
@@ -0,0 +1,89 @@
+/*var data = [4, 8, 15, 16, 23, 42];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
+ */
+/*
+var graph = {
+    "nodes":[
+	{"name":"A","group":1},
+	{"name":"B","group":1}
+    ],
+    "links":[
+	{"source":0,"target":1,"value":1}
+    ]
+};
+ */
+
+var graph = {
+    "nodes":[
+	{"name":"Brian","group":1,"added":1},
+	{"name":"Alla","group":2,"added":2},
+	{"name":"Boris","group":2,"added":2}
+    ],
+    "links":[
+	{"source":1,"target":0,"value":1},
+	{"source":2,"target":0,"value":1}
+    ]
+};
+
+var width = 960,
+    height = 500;
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+	.charge(-120)
+	.linkDistance(30)
+	.size([width, height]);
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+//d3.json("data.json", function(error, graph) {
+force
+    .nodes(graph.nodes)
+    .links(graph.links)
+    .start();
+
+var link = svg.selectAll(".link")
+	.data(graph.links)
+	.enter().append("line")
+	.attr("class", "link")
+	.style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+var node = svg.selectAll(".node")
+	.data(graph.nodes)
+	.enter().append("circle")
+	.attr("class", "node")
+	.attr("r", 5)
+	.attr("name", function(d) {return d.name;})
+	.style("fill", function(d) { return color(d.group); })
+	.call(force.drag)
+	.on("mousedown",function(){
+	    console.log(d3.select(this).attr("name"));
+	});
+
+node.append("title")
+    .text(function(d) { return d.name; });
+
+force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+});
+//});
+
